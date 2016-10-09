f9ed9f0bcaade48c691f08c29f624a7aa0ed8362
Julia Wlo <Julia Wlo>
2016-10-08 20:54:37 -0400

    added blinky text to menus

--- DeadState.hx
+++ DeadState.hx
@@ -10,20 +10,38 @@ import flixel.math.FlxMath;
 class DeadState extends FlxState
 {
 	var prettyImage:FlxSprite;
+	var blinkingText:FlxSprite;
+	var timer:Int = 0;
+	var wait:Int = 5;
 	override public function create():Void
 	{
 		prettyImage = new FlxSprite();
 		prettyImage.loadGraphic(AssetPaths.deadScreenAnimation__png, true, 256, 144);
-		prettyImage.animation.add("dead", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 30, true);
+		prettyImage.animation.add("dead", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 7, true);
 		prettyImage.animation.play("dead");
 		add(prettyImage);
+		
+		blinkingText = new FlxSprite();
+		blinkingText.loadGraphic(AssetPaths.dead_pressKey__png);
+		blinkingText.x = FlxG.width / 2 - blinkingText.width / 2;
+		blinkingText.y = FlxG.height * 4 / 5;
+		add(blinkingText);
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		Ctrl.update();
-		if (Ctrl.anyJustPressed)
+		timer = (timer + 1) % 20;
+		if (timer == 0)
+		{
+			blinkingText.visible = !blinkingText.visible;
+		}
+		if (wait > 0)
+		{
+			wait--;
+		}
+		else if (Ctrl.anyJustPressed)
 		{
 			// go to menu state
 			FlxG.switchState(new MenuState());
--- MenuState.hx
+++ MenuState.hx
@@ -10,6 +10,11 @@ import flixel.math.FlxMath;
 class MenuState extends FlxState
 {
 	var prettyImage:FlxSprite;
+	var blinkingText:FlxSprite;
+	var timer:Int = 0;
+	// I can't fix the key just pressed bug, so we'll just force you to wait a bit when the scene loads
+	var wait:Int = 5;
+	
 	override public function create():Void
 	{
 		prettyImage = new FlxSprite();
@@ -17,16 +22,31 @@ class MenuState extends FlxState
 		prettyImage.animation.add("title", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, true);
 		prettyImage.animation.play("title");
 		add(prettyImage);
+		
+		blinkingText = new FlxSprite();
+		blinkingText.loadGraphic(AssetPaths.insertcoin__png);
+		blinkingText.x = FlxG.width / 2 - blinkingText.width / 2;
+		blinkingText.y = FlxG.height * 3 / 4;
+		add(blinkingText);
 		super.create();
 	}
 	override public function update(elapsed:Float):Void
 	{
+		timer = (timer + 1) % 20;
+		if (timer == 0)
+		{
+			blinkingText.visible = !blinkingText.visible;
+		}
 		Ctrl.update();
-	if (Ctrl.anyJustPressed )
-	{
+				if (wait > 0)
+		{
+			wait--;
+		}
+		else if (Ctrl.anyJustPressed)
+		{
 		// go to play state
 		FlxG.switchState(new PlayState());
-	}
+		}
 		super.update(elapsed);
 	}
 }
